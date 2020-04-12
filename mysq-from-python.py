85a611caa496c05470dfc78f58ec26cef80d9def
robibrutoczki <robibrutoczki@googlemail.com>
2020-04-12 08:04:33 +0000

    UPDATE function

--- mysq-from-python.py
+++ mysq-from-python.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -14,10 +14,8 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        row = [("Bob", 21, "1990-02-06 23:04:56"),
-                ("Jim", 56, "1955-05-09 13:12:45"),
-                ("Fred", 100, "1911-09-12 01:01:01")]
-        cursor.executemany("INSERT INTO Friends VALUES(%s, %s, %s);",row)
+      
+        cursor.execute("UPDATE Friends SET age=22 WHERE name='Bob';")
         connection.commit()
         # INSERTING MANY ROW into a data tables 
         # NOTE:dont forget the connection.EXECUTEMANY
