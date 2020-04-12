864e42122a0c71d45340477b1525db4321504224
robibrutoczki <robibrutoczki@googlemail.com>
2020-04-12 08:12:21 +0000

    UPDATE MANY function

--- mysq-from-python.py
+++ mysq-from-python.py
@@ -1,5 +1,4 @@
 import os
-
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -11,15 +10,13 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-
 try:
     with connection.cursor() as cursor:
-      
-        cursor.execute("UPDATE Friends SET age=%s WHERE name=%s;",
-                         (23,"Bob"))
+        rows = [(23, 'bob'),
+                (24, 'jim'),
+                (25, 'fred')]
+        cursor.executemany("UPDATE Friends SET age = %s WHERE name = %s;",
+                           rows)
         connection.commit()
-        # INSERTING MANY ROW into a data tables 
-        # NOTE:dont forget the connection.EXECUTEMANY
-        # table already exists
 finally:
     connection.close()
